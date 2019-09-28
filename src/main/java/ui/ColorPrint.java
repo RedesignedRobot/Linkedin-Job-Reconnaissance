7b769d89ebd32fa3ee8494f48440863cdab24c77
Amir Ayub <dev.amirayub@gmail.com>
2019-09-28 15:33:03 +0300

    removed ansi color, updated jar

--- src/main/java/ui/ColorPrint.java
+++ src/main/java/ui/ColorPrint.java
@@ -2,27 +2,27 @@ package ui;
 
 public class ColorPrint {
     public void red(String s){
-        System.out.println(ConsoleColors.RED_BRIGHT + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void green(String s){
-        System.out.println(ConsoleColors.GREEN_BRIGHT + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void blue(String s){
-        System.out.println(ConsoleColors.BLUE_BRIGHT + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void yellow(String s){
-        System.out.println(ConsoleColors.YELLOW_BRIGHT + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void redU(String s){
-        System.out.println(ConsoleColors.RED_UNDERLINED + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void greenU(String s){
-        System.out.println(ConsoleColors.GREEN_UNDERLINED + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void blueU(String s){
-        System.out.println(ConsoleColors.BLUE_UNDERLINED + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
     public void yellowU(String s){
-        System.out.println(ConsoleColors.YELLOW_UNDERLINED + s + ConsoleColors.RESET);
+        System.out.println(s);
     }
 }
---
+++ src/main/java/ui/ColorPrint_BROKEN.java
@@ -0,0 +1,29 @@
+package ui;
+
+@Deprecated
+public class ColorPrint_BROKEN {
+    public void red(String s){
+        System.out.println(ConsoleColors.RED_BRIGHT + s + ConsoleColors.RESET);
+    }
+    public void green(String s){
+        System.out.println(ConsoleColors.GREEN_BRIGHT + s + ConsoleColors.RESET);
+    }
+    public void blue(String s){
+        System.out.println(ConsoleColors.BLUE_BRIGHT + s + ConsoleColors.RESET);
+    }
+    public void yellow(String s){
+        System.out.println(ConsoleColors.YELLOW_BRIGHT + s + ConsoleColors.RESET);
+    }
+    public void redU(String s){
+        System.out.println(ConsoleColors.RED_UNDERLINED + s + ConsoleColors.RESET);
+    }
+    public void greenU(String s){
+        System.out.println(ConsoleColors.GREEN_UNDERLINED + s + ConsoleColors.RESET);
+    }
+    public void blueU(String s){
+        System.out.println(ConsoleColors.BLUE_UNDERLINED + s + ConsoleColors.RESET);
+    }
+    public void yellowU(String s){
+        System.out.println(ConsoleColors.YELLOW_UNDERLINED + s + ConsoleColors.RESET);
+    }
+}
